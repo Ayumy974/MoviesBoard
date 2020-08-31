d3685c6d560972b3d1a1d3b97f7e0439dbecea93
fklock <francoiseklock@gmail.com>
2020-08-31 20:04:20 +0200

    MovieCard: conditional display of the movie presentation when the mouse hovers over it.

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './movieCard.module.scss';
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
+    const [display, setDisplay] = useState(false)
+
+    const handleMouseEnter = () => setDisplay(true);
+    const handleMouseLeave = () => setDisplay(false);
+
     return (
-        <div className={style.card}>
-            <img className={style.cardImage} src={movie.poster} alt={movie.title}/>
-            <div className={style.cardContent}>
-                <h3 className={style.cardTitle}>{movie.title}</h3>
-                <p>Date de sortie: {movie.release_date}</p>
-                <p className={style.cardDesc}>Synopsis: {movie.description}</p>
-            </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={style.card}>
+            <img className={style.cardImage} src={movie.poster} alt={movie.title} />
+            {display && 
+                <div className={style.cardContent}>
+                    <h3 className={style.cardTitle}>{movie.title}</h3>
+                    <p>Date de sortie: {movie.release_date}</p>
+                    <p className={style.cardDesc}>Synopsis: {movie.description}</p>
+                </div>
+            }
         </div>
     )
 }
