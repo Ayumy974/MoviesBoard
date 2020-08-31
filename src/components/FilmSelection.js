775053f6e9f3e7b67576eb46e9aecd513797482a
fklock <francoiseklock@gmail.com>
2020-08-31 15:54:03 +0200

    creation: component folders and pages / component FilmSelection / page Movie component MoviesList / component MovieCard.

---
+++ src/components/FilmSelection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FilmSelection = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FilmSelection
---
+++ src/pages/Movies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FilmSelection from '../components/FilmSelection';
+import MoviesList from '../components/FilmSelection';
+
+const Movies = () => {
+    return (
+        <section>
+            <FilmSelection />
+            <MoviesList />
+        </section>
+    )
+}
+
+export default Movies
